6f9cfc5df40a7026e058a1381ddc76dfec710c49
yohu12 <82370288@qq.com>
2018-11-06 23:00:34 +0800

    提交

---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="wrapper" class="bg-all">
+        <h2>Hello world</h2>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'landing-page',
+        methods: {
+            open(link) {
+                this.$electron.shell.openExternal(link)
+            }
+        }
+    }
+</script>
+
+<style>
+    .bg-all{
+        background: aqua;
+    }
+    h2{
+
+    }
+</style>
